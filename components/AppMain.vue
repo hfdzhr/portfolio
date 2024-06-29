9c1daec55d9203bc86f92e549cdd805369abea70
hfdzhr <21221753116@smkassalaambandung.sch.id>
2024-06-29 15:52:38 +0700

    style(animation): add GSAP animation on hero section

--- components/AppMain.vue
+++ components/AppMain.vue
@@ -8,19 +8,29 @@
               class="text-5xl sm:text-5xl md:text-6xl lg:text-7xl font-ws font-semibold"
               id="name"
             >
-              Hafid Al Azhar
+              <span class="text-wrapper">
+                <span
+                  v-for="(letter, index) in letters"
+                  :key="index"
+                  class="letter"
+                >
+                  {{ letter === ' ' ? '\u00A0' : letter }}
+                </span>
+              </span>
             </h1>
-            <p class="py-6 text-sm sm:text-lg font-ws">
+            <p class="py-6 text-sm sm:text-lg font-ws" id="subtitle">
               Web Developer | Tech Enthusiast | Student
             </p>
           </div>
         </div>
         <div class="absolute bottom-16 left-1/2 transform -translate-x-1/2">
-          <Icon
-            class="animate-bounce rotate-0"
-            name="streamline:interface-arrows-button-down-double-arrow-arrows-double-down"
-            size="35px"
-          />
+          <div id="icon-container">
+            <Icon
+              class="animate-bounce rotate-0"
+              name="streamline:interface-arrows-button-down-double-arrow-arrows-double-down"
+              size="35px"
+            />
+          </div>
         </div>
       </div>
     </section>
@@ -272,12 +282,52 @@
 <script setup>
 import { Form, Field, ErrorMessage } from 'vee-validate';
 import { toTypedSchema } from '@vee-validate/zod';
+import { gsap } from 'gsap';
 import * as zod from 'zod';
 
 const myEmail = 'hafidalazhar5@gmail.com';
 const fullName = ref('');
 const subject = ref('');
 const message = ref('');
+const text = 'Hafid Al Azhar';
+const letters = ref(text.split(''));
+
+onMounted(() => {
+  const tl = gsap.timeline();
+
+  tl.fromTo(
+    '.letter',
+    { y: '100%' },
+    {
+      y: '0%',
+      duration: 2,
+      stagger: 0.03,
+      ease: 'power4.out',
+    }
+  )
+    .fromTo(
+      '#subtitle',
+      { opacity: 0, y: 50 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        ease: 'power4.out',
+      },
+      '-=1'
+    )
+    .fromTo(
+      '#icon-container',
+      { opacity: 0, y: 20 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        ease: 'power4.out',
+      },
+      '-=0.2'
+    );
+});
 
 const validationSchema = toTypedSchema(
   zod.object({
@@ -303,3 +353,16 @@ const submit = (values) => {
   window.location.href = mailtoLink;
 };
 </script>
+
+<style scoped>
+.text-wrapper {
+  display: inline-block;
+  overflow: hidden;
+  vertical-align: bottom;
+}
+
+.letter {
+  display: inline-block;
+  transform: translateY(100%);
+}
+</style>
